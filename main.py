fe8d1e5700364f62c36fd3bc7ee2a93206dfc873
Simon Corde <scorde@MacBook-Pro-de-Simon.local>
2025-06-29 15:51:35 +0200

    add first bunch of code working locally

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+from utils import run_query
+
+GCP_PROJECT_ID = os.getenv("GCP_PROJECT_ID")
+INPUT_DATASET = os.getenv("INPUT_DATASET")
+OUTPUT_DATASET = os.getenv("OUTPUT_DATASET")
+OUTPUT_TABLE = os.getenv("OUTPUT_TABLE")
+
+
+def process():
+    run_query(
+        project_id=GCP_PROJECT_ID,
+        input_dataset=INPUT_DATASET,
+        output_dataset=OUTPUT_DATASET,
+        output_table=OUTPUT_TABLE,
+        sql_file="query.sql",
+    )
+
+    return "200"
+
+
+if __name__ == "__main__":
+    status = process()
+    print(f"Process completed with status: {status}")
---
+++ utils.py
@@ -0,0 +1,71 @@
+import logging
+from google.cloud import bigquery
+from jinja2 import Template
+
+
+def get_context(
+    project_id: str, dataset: str, table_stations: str, table_capacity: str
+) -> dict:
+    """
+    Retrieves the context for Jinja template rendering.
+
+    Returns:
+        dict: Context variables including project ID, dataset, and table.
+    """
+    return {
+        "project_id": project_id,
+        "dataset": dataset,
+        "table_stations": table_stations,
+        "table_capacity": table_capacity,
+    }
+
+
+def run_query(
+    project_id: str,
+    input_dataset: str,
+    output_dataset: str,
+    output_table: str,
+    sql_file: str,
+) -> str:
+    """
+    Executes a BigQuery SQL query after parsing it with Jinja.
+
+    Args:
+        project_id (str): GCP project ID.
+        dataset (str): BigQuery dataset name.
+        output_table (str): BigQuery table name.
+        sql_file (str): Path to the SQL file.
+        context (dict): Context variables for Jinja template rendering.
+
+    Returns:
+        str: Status code ("200" for success).
+    """
+    # Read the SQL file
+    with open(file=sql_file, mode="r") as file:
+        sql_template = file.read()
+
+    # Render the SQL template with the provided context
+    context = get_context(
+        project_id=project_id,
+        dataset=input_dataset,
+        table_stations="stations",
+        table_capacity="capacity",
+    )
+    template = Template(sql_template)
+    sql_query = template.render(context)
+
+    print(f"Executing query: {sql_query}")
+
+    table_id = f"{project_id}.{output_dataset}.{output_table}"
+
+    job_config = bigquery.QueryJobConfig(
+        destination=table_id, write_disposition="WRITE_APPEND"
+    )
+
+    bigquery_client = bigquery.Client(project=project_id)
+    job = bigquery_client.query(query=sql_query, job_config=job_config)
+    job.result()
+
+    logging.info(msg=f"Data written at: {project_id}.{output_dataset}.{output_table}")
+
+    return "200"
