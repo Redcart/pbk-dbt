e26b80e9c1f79106918f6a9a04b003254e5c0853
Simon Corde <scorde@MacBook-Pro-de-Simon.local>
2025-07-20 11:33:33 +0200

    fix sql

--- src/query_bikes_aggregated.sql
+++ src/query_bikes_aggregated.sql
@@ -3,6 +3,6 @@ SELECT
   SUM(nb_bikes_available_electric) AS sum_nb_bikes_available_electric,
   SUM(nb_bikes_available) AS sum_nb_bikes_available,
   ingestion_time
-FROM  `{{ project_id }}.{{ dataset }}.{{ table_stations }}`
+FROM  `{{ project_id }}.{{ dataset }}.{{ table_capacity }}`
 GROUP BY ingestion_time
 ORDER BY ingestion_time
